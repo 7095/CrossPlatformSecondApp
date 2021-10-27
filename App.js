5cfa19be22796f8c47fe5266cdf0cf45bdbfaf45
Uzair Ali <uzair.aali@hotmail.com>
2021-10-27 11:56:02 +1100

    config.js not fully working

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//Components
+import { Signup } from './components/Signup';
+import { Signin } from './components/Signin';
+import { Home } from './components/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Signup" component={Signup}/>
+        <Stack.Screen name="Signin" component={Signin}/>
+        <Stack.Screen name="Home" component={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
